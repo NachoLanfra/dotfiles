931f499b23b2cd23c8ea54a5271375a40919eac0
NachoLanfra <ilanfranconi@alumnos.exa.unicen.edu.ar>
2026-07-02 16:54:33 -0300

    fix: ruta de wallpaper corregida en autostart.lua; packages: agrego colloid-gtk-theme-git; docs: notas para install.sh

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -5,5 +5,5 @@ local terminal = "kitty"
 hl.on("hyprland.start", function()
     hl.exec_cmd(terminal)
     hl.exec_cmd("nm-applet")
-    hl.exec_cmd("swayosd-server & waybar > /dev/null 2>&1 & swaybg -i /home/nachohyperland/Pictures/wallpaper.jpg -m fill & /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &")
+    hl.exec_cmd("swayosd-server & waybar > /dev/null 2>&1 & swaybg -i /home/hyprlandnacho/Pictures/wallpaper.jpg -m fill & /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &")
 end)
