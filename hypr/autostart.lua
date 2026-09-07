60ae95aa5b70c787675238099f53da6f927d3115
NachoLanfra <ilanfranconi@alumnos.exa.unicen.edu.ar>
2026-09-06 21:12:11 -0300

    cambios de paquetes y aplicaciones varias

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -4,7 +4,6 @@ local terminal = "kitty"
 
 hl.on("hyprland.start", function()
     hl.exec_cmd(terminal)
-    hl.exec_cmd("nm-applet")
-    hl.exec_cmd("swayosd-server & waybar > /dev/null 2>&1 & swaybg -i /home/hyprlandnacho/Pictures/wallpaper.jpg -m fill & /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &")
+	hl.exec_cmd("nm-applet & swayosd-server & waybar > /dev/null 2>&1 & swaybg -i /home/hyprlandnacho/Pictures/wallpaper.jpg -m fill & /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & hypridle &")
     hl.exec_cmd("sh -c 'sleep 1.5 && swayosd-client --output-volume raise && swayosd-client --output-volume lower' &")
 end)
